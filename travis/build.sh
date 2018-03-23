996aac0f500d3947f9ae3e0f14c2d6adf72def2c
Housni Yakoob <housni.yakoob@gmail.com>
2018-03-23 18:08:53 +0300

    Adding deploy phase, moved build steps to shell script

---
+++ travis/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# Enhance debugging by expanding and showing shell commands
+# set -x
+
+# Credit to Stack Overflow questioner Jiarro and answerer Dave Dopson
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+# http://stackoverflow.com/a/246128/424301
+declare DIR
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+yarn install
+ionic cordova platform add android --nofetch
+ionic cordova build android
