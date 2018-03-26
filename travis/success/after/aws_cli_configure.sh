c82e66de990a4266fe81eacbc36b4026b87c5632
Housni Yakoob <housni.yakoob@gmail.com>
2018-03-26 16:28:32 +0300

    Insignificant changes to generate a test build

--- travis/success/after/aws_cli_configure.sh
+++ travis/success/after/aws_cli_configure.sh
@@ -14,11 +14,19 @@ declare DIR
 # shellcheck disable=SC2034
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# TODO: Use `aws iam get-user` to display more info.
-echo "Uploading build from branch '$TRAVIS_BRANCH' to S3 bucket '$S3_BUILD_BUCKET'"
+# Set up AWS CLI so that we can invoke AWS Device Farm tests.
+# Credits: https://stackoverflow.com/a/44850245/379786
+mkdir -p "${HOME}"/.aws
 
-# Move the .apk file to a dir on its own since the entire dir will be uploaded
-# to the S3 bucket.
-mv \
-    "${ANDROID_BUILD_DIR}"/android-debug.apk \
-    "${ANDROID_BUILD_LATEST_DIR}"/"${ANDROID_DEBUG_APK_NAME}"
+cat > "${HOME}"/.aws/credentials << EOL
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOL
+
+# Device Farm is only available in region 'us-west-2'
+# See: https://docs.aws.amazon.com/general/latest/gr/rande.html#devicefarm_region
+cat > "${HOME}"/.aws/config << EOL
+[default]
+region = us-west-2
+EOL
