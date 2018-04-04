38c1075f80fefd8142afb06a61e92ef7968aebdd
Housni Yakoob <housni.yakoob@gmail.com>
2018-04-04 04:00:25 +0300

    Manually installing nvm and speeding it up

--- ci/install/dependencies.sh
+++ ci/install/dependencies.sh
@@ -14,7 +14,13 @@ declare DIR
 # shellcheck disable=SC2034
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Source: http://yoember.com/nodejs/the-best-way-to-install-node-js/#on-linux
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+
 # v8 is the active LTS release.
 nvm install 8
+node -v
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
 yarn global add ionic
 yarn global add cordova
