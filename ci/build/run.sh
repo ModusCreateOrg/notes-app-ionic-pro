8d35c3aa85bf5348992811a76869d6cb34ddb9f0
Housni Yakoob <housni.yakoob@gmail.com>
2018-04-04 03:41:21 +0300

    Testing minimal dependencies for Travis

--- ci/build/run.sh
+++ ci/build/run.sh
@@ -17,6 +17,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 yarn install
 # TODO: Figure out why we have to remove android and add it for this to work.
 # See: https://travis-ci.org/ModusCreateOrg/notes-app-ionic-pro/builds/359512339#L3329
-ionic cordova platform remove android
-ionic cordova platform add android --nofetch
+# ionic cordova platform remove android
+# ionic cordova platform add android --nofetch
+ionic cordova platform add android
 ionic cordova build android
---
+++ ci/install/dependencies.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# Enhance debugging by expanding and showing shell commands
+# set -x
+
+# Credit to Stack Overflow questioner Jiarro and answerer Dave Dopson
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+# http://stackoverflow.com/a/246128/424301
+declare DIR
+# shellcheck disable=SC2034
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# v8 is the active LTS release.
+nvm install 8
+yarn global add ionic
+yarn global add cordova
